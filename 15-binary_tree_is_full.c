ede8dd52436479b2c8da64f956cb0c257a12cf40
Hassan Cissey <hcissey0@gmail.com>
2023-08-31 03:14:56 +0000

    Is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,24 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * tree_nodes - Counts the number of nodes in a tree
- * @tree: The tree to be counted
- *
- * Return: The number of nodes or 0
- */
-size_t tree_nodes(const binary_tree_t *tree)
-{
-	size_t left, right;
-
-	if (tree == NULL)
-		return (0);
-	left = tree_nodes(tree->left);
-	right = tree_nodes(tree->right);
-
-	return (1 + left + right);
-}
-
-/**
  * binary_tree_is_full - Checks if a binary tree is full
  * @tree: The tree to check through
  *
@@ -26,12 +8,9 @@ size_t tree_nodes(const binary_tree_t *tree)
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int left, right;
-
 	if (tree == NULL)
 		return (0);
-	left = tree_nodes(tree->left);
-	right = tree_nodes(tree->right);
-
-	return (left == right);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
